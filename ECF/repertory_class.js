2fd11b4b725a389c480a7e06236ddb56fdcfc5a0
Belén <etudebelen84@gmail.com>
2026-03-23 15:51:04 +0100

    Start class repertory JS

---
+++ ECF/repertory_class.js
@@ -0,0 +1,14 @@
+class pokemon {
+  constructor(imgURL, name, numPok, type, country, ptLife, ptAttack, ptDef, evolution, description) {
+    this.imgURL = imgURL;
+    this.name = name;
+    this.numPok = numPok;
+    this.type = type;
+    this.country = country;
+    this.ptLife = ptLife;
+    this.ptAttack = ptAttack;
+    this.ptDef = ptDef;
+    this.evolution = evolution;
+    this.description = description;
+  }
+}
