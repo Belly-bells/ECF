0b1df828868636b80e5b071b76b58e24f8c24cd7
Belén <etudebelen84@gmail.com>
2026-03-25 09:04:43 +0100

    Start three days ECF

--- JS/Repertoire/callAPI.js
+++ JS/Repertoire/callAPI.js
@@ -38,19 +38,19 @@ export const searchTrue = async (monInput, searchType) => {
 };
 //Variable OFFSET pour le point de départ du rechargement de la page = extérieure de la constante loadPokemon pour son scope
 let offset = 0; //Point de départ
-const LIMIT = 0; //La limite ne changera pas.
+const limit = 20; //La limite ne changera pas
 //Appel API pour le chargement de la page d'acceuil
 export const loadPokemon = async () => {
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
     const data = await response.json();
-    offset += LIMIT; //Réimplantaton de la variable limite dans la variable "offset" afin qu'elle devienne le point de départ du prochain chargement
+    offset += limit; //Réimplantaton de la variable limite dans la variable "offset" afin qu'elle devienne le point de départ du prochain chargement
     return data.results;
   } catch (erreur) {
     searchFalse(erreur.message);
   }
 };
-//
+//Affichage page d'acceuil : constante appel API pour l'affichage des pokémon
 export const fetchPokemonDetails = async (url) => {
   //url est vide + attente d'un paramétre
   try {
@@ -67,5 +67,5 @@ export const fetchPokemonDetails = async (url) => {
 };
 
 export const loadBatch = async () => {
-  const list = await fetchPokemonList();
+  const list = await loadPokemon();
 };
