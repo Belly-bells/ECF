fd58f692141eaee9f52f82bc398fe648503e6796
Belén <etudebelen84@gmail.com>
2026-03-25 15:59:06 +0100

    Big party JS finish

--- JS/teams.js
+++ JS/teams.js
@@ -1,2 +1,9 @@
 import { pokemonScreen } from "./Repertoire/repertory_class.js";
+
 const equipe = JSON.parse(localStorage.getItem("equipe")) || [];
+for (const elt of equipe) {
+  //instanciation
+  const affPok = new pokemonScreen(elt.name, elt.image, elt.number, elt.categories);
+
+  affPok.getCardHTML();
+}
